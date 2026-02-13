106f6d7e903e06fc6bfa98a91a82523321787494
Arya Akbari <arya.akbari.nodehi89@gmail.com>
2026-02-13 14:53:41 +0330

    Add files via upload

---
+++ writer.py
@@ -0,0 +1,14 @@
+class book:
+    
+    def __init__(writer,nameofthebook,سبک):
+        
+      self.writer=writer
+      
+      self.namethebook=nameofthebook
+      
+      self.سبک=سبک
+      
+      def __str__(writer_21):
+      
+  writer_21=person("Abas marofee",سبک ژانر)
+     print(writer_21)
